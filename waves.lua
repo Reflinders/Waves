1803b6a07a053c1ce41afebdf91f793e5112a956
Reflinders <74808818+Reflinders@users.noreply.github.com>
2023-06-19 12:30:24 -0500

    Update waves.lua
    v.1.3.2
    - typified wave signals and connections
    - added new arg for `Wait` called `meetsConditions`. when given, the wait will only finish off if the function `meetsConditions` returns `true`.

--- waves.lua
+++ waves.lua
@@ -1,4 +1,4 @@
--- new and update version; v.1.3.1, Updated 5/30/23
+-- new and update version; v.1.3.2, U-6/19/23
 local wave = {}; wave.__index = wave
 local connector = require(script.Connector)
 --/ ...
@@ -7,14 +7,20 @@ function wave:Destroy()
 		con:Disconnect()
 	end
 end
-function wave:Connect(func : (any?) -> (any?))
+function wave:Connect(func : (any?) -> (any?)) : connector.Connection
 	local nConnector = connector.new(func)
 	self.con[#self.con + 1] = nConnector; return nConnector
 end
-function wave:Wait()
-	-- waits until a connection is fired
-	local fired; local newConnection = self:Connect(function()
-		fired = true
+function wave:Wait(meetsConditions : ()->()|nil) : boolean
+	-- waits until a connection is fired with met conditions
+	local fired; local newConnection = self:Connect(function(...)
+		if meetsConditions then
+			if meetsConditions(...) then
+				fired = true
+			end
+		else
+			fired = true
+		end
 	end)
 	while (self) and (not fired) do
 		task.wait(1/30)
@@ -29,7 +35,10 @@ function wave:Fire(...)
 	end
 end
 -- ... 
-function wave.new()
+export type Signal = {
+	con : {connector.Connection}, -- all the connections
+} & typeof(setmetatable({}, wave))
+function wave.new() : Signal
 	local newWave = setmetatable({con = {}}, wave)
 	return newWave
 end
